683079007daa817c2b19ad74d0b65716dfca28ea
Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>
2020-10-08 19:49:59 +0300

    BJ-973: Initial

---
+++ src/test/java/com/rbkmoney/absolutely/AbsolutelyApplicationTest.java
@@ -0,0 +1,16 @@
+package com.rbkmoney.absolutely;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AbsolutelyApplication.class)
+public class AbsolutelyApplicationTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
